0639ce576377efd3a17f31dd0eb5f586760b2550
Richard Easton <richeaston@users.noreply.github.com>
2018-03-28 11:22:24 +0100

    Made paths and filenames match shutdown.ps1

--- export PC list.ps1
+++ export PC list.ps1
@@ -1,3 +1,4 @@
-﻿$path = "C:\\AllComputersInOU.txt"
-Get-ADComputer -Filter * -SearchBase "OU=[pc'sOU],DC=domain,DC=org" -Properties *  | Select -Property Name | Sort-Object -Property name, ws | Out-File $path 
-[System.Windows.MessageBox]::Show("PC list exported to $path")
+﻿$basepath = "c:\\shutdown"
+$file = "$basepath\clist.txt"
+Get-ADComputer -Filter * -SearchBase "OU=[pc'sOU],DC=domain,DC=org" -Properties *  | Select -Property Name | Sort-Object -Property name, ws | Out-File $file 
+[System.Windows.MessageBox]::Show("PC list exported to $file")
